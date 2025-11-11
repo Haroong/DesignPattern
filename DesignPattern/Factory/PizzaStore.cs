9bb324595b742173b5a5b0ccc3de2cf7033fd45f
조혜륜 <absolutecool18@gmail.com>
2025-11-11 17:16:23 +0900

    feat #4 SimplePizzaFactory를 이용해서 new 연산자 대신 객체 메소드 호출

---
+++ DesignPattern/Factory/PizzaStore.cs
@@ -0,0 +1,27 @@
+﻿namespace DesignPattern.Factory;
+
+/// <summary>
+///     피자 주문 프로세스를 관리하는 클라이언트 클래스
+/// </summary>
+public class PizzaStore
+{
+    private readonly SimplePizzaFactory _factory;
+
+    public PizzaStore(SimplePizzaFactory factory)
+    {
+        _factory = factory;
+    }
+
+    public Pizza orderPizza(string type)
+    {
+        Pizza pizza;
+        pizza = _factory.CreatePizza(type);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+}
---
+++ DesignPattern/Factory/SimplePizzaFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPattern.Factory;
+
+/// <summary>
+///     타입에 따른 피자 생성 팩토리
+/// </summary>
+public class SimplePizzaFactory
+{
+    public Pizza CreatePizza(string type)
+    {
+        Pizza pizza = null; // TODO 추후 구현
+
+        if (type.Equals("Cheese"))
+            pizza = new CheesePizza();
+        else if (type.Equals("Peperoni")) pizza = new PeperoniPizza();
+        else if (type.Equals("Veggie")) pizza = new VeggiePizza();
+
+        return pizza;
+    }
+}
