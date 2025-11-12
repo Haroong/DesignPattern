737dc57fac7b12f48177b7299200b5a1f0646cc3
조혜륜 <absolutecool18@gmail.com>
2025-11-12 18:36:12 +0900

    feat #4 Pizza 클래스 생성

---
+++ DesignPattern/Factory/Pizza.cs
@@ -0,0 +1,33 @@
+﻿namespace DesignPattern.Factory;
+
+public abstract class Pizza
+{
+    private string _dough;
+    private string _name;
+    private readonly List<string> _toppings = new();
+    private string sauce;
+
+    public void prepare()
+    {
+        Console.WriteLine($"{_name} 피자 준비 중...");
+        Console.WriteLine("도우를 돌리는 중...");
+        Console.WriteLine("소스를 뿌리는 중...");
+        Console.WriteLine("토핑을 올리는 중: ");
+        foreach (var topping in _toppings) Console.WriteLine($"  {topping}");
+    }
+
+    public void bake()
+    {
+        Console.WriteLine("175도의 오븐에서 25분 동안 피자 굽기");
+    }
+
+    public void cut()
+    {
+        Console.WriteLine("피자를 사선으로 자르기");
+    }
+
+    public void box()
+    {
+        Console.WriteLine("상자에 피자 담기");
+    }
+}
